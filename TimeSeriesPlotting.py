b268ed78b3c7d2226871c405138e9f1c1e3af7cb
Adrián Carro <adrian.carro@gmail.com>
2019-05-27 14:16:26 +0100

    Update time series plotting for this model

--- TimeSeriesPlotting.py
+++ TimeSeriesPlotting.py
@@ -6,30 +6,49 @@ import matplotlib.pyplot as plt
 
 
 # Control variables
-temperature = 0.24
-nAgents = 1000
+temperature1 = 0.1
+temperature2 = 10.0
+fraction_of_agents1 = 0.5
+delta = 0.01
+n_agents = 1000
+final_time = 10000
 
-# Reading model results from file
-with open("./Results/nAgentsUp-T{:.4f}.csv".format(temperature), 'r') as f:
-    line = f.next()
-    nAgentsUp = [[] for i in range(len(line.split(",")))]
-    for column, element in zip(nAgentsUp, line.split(",")):
-        column.append(float(element))
+# Reading model results from files
+rate = []
+with open("./Results/profitRateC-delta{:.4f}-T{:.4f}-T{:.4f}.csv".format(delta, temperature1, temperature2), "r") as f:
     for line in f:
-        for column, element in zip(nAgentsUp, line.split(",")):
-            column.append(float(element))
+        rate.append(float(line))
+n_agents_up1 = []
+n_agents_up2 = []
+with open("./Results/nAgentsUpC-delta{:.4f}-T{:.4f}-T{:.4f}.csv".format(delta, temperature1, temperature2), "r") as f:
+    for line in f:
+        n_agents_up1.append(float(line.split(",")[0]))
+        n_agents_up2.append(float(line.split(",")[1]))
 
-# Plot
-fig = plt.figure(figsize=(8, 6), facecolor='white')
-plt.plot(range(len(nAgentsUp[0])), nAgentsUp[8], "o-", label="nAgentsUp")
-plt.xlim(0.0, len(nAgentsUp[0]))
-plt.ylim(0.0, nAgents)
-plt.ylabel("nAgentsUp")
+# Create figure
+plt.figure(figsize=(6, 4.5), facecolor='white')
+# Plot number of agents up
+plt.plot(range(final_time + 1), [x / (n_agents * fraction_of_agents1) for x in n_agents_up1], "-b",
+         label="n_agents_up1")
+plt.plot(range(final_time + 1), [x / (n_agents * (1 - fraction_of_agents1)) for x in n_agents_up2], "-g",
+         label="n_agents_up2")
+plt.xlim(0.0, final_time)
+plt.ylim(0.0, 1.0)
+plt.ylabel("Frequency of Action (for each type of agent)")
 plt.xlabel("Time")
-plt.title("T = " + str(temperature))
-# plt.legend()
-plt.tight_layout()
-# plt.savefig("./nAgentsUp-T{:.4f}.eps".format(temperature), format='eps', dpi=300, bbox_inches='tight')
+plt.legend()
+plt.title(r"$\delta$ = {:.4f}, T1 = {:.4f}, T2 = {:.4f}".format(delta, temperature1, temperature2))
+# Plot rate of profit
+ax1 = plt.gca()
+ax1.twinx()  # instantiate a second axes that shares the same x-axis
+plt.plot(range(final_time + 1), rate, "-r", label="rate")
+plt.axhline(y=0, ls="--", c="k", lw=2)
+plt.xlim(0.0, final_time)
+plt.ylim(-delta * 100, delta * 100)
+plt.ylabel("Rate of profit")
 
-# Show
+# Show and save
+plt.tight_layout()
 plt.show()
+# plt.savefig("./RateTimeSeriesC-delta{:.4f}-T{:.4f}-T{:.4f}.pdf".format(delta, temperature1, temperature2),
+#             format="pdf", dpi=300, bbox_inches='tight')
